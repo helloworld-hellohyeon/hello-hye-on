3d435f68417999d01b126e0f0fa12b5da7194960
권현수 <hellohyeon@mibank.me>
2021-02-05 10:05:19 +0900

    scroll animation 설정

--- src/Component/Title/Title.js
+++ src/Component/Title/Title.js
@@ -1,37 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import className from "classnames/bind";
 import style from "./Title.scss";
+import { setInterval } from "timers";
 
 const cx = className.bind(style);
 
 const Title = () => {
-  const [currentEle, setCurrentEle] = useState(null);
-  let i = 0;
+  const title = [
+    "타이틀1",
+    "타이틀2",
+    "타이틀3",
+    "타이틀4",
+    "타이틀5",
+    "타이틀6",
+    "타이틀7",
+    "타이틀8",
+    "타이틀9",
+    "타이틀10",
+  ];
+  const [activeIdx, setActiveIdx] = useState(0);
+  const idxRef = useRef(0);
 
   useEffect(() => {
-    setCurrentEle(document.querySelector(".active"));
+    const animationInterval = setInterval(() => {
+      if (idxRef.current < 9) {
+        idxRef.current += 1;
+      } else {
+        idxRef.current = 0;
+      }
+      setActiveIdx(idxRef.current);
+    }, 2000);
+
+    return () => clearInterval(animationInterval);
   }, []);
-
-  useEffect(() => {
-    console.log(currentEle);
-  }, [currentEle]);
 
   return (
     <div className={cx("title-wrapper")}>
-      <div className={cx("fixed-title")}>이것은</div>
-      <div className={cx("scroll-title-box")}>
-        <div className={cx("scroll-title", "active")}>스크롤타이틀1</div>
-        <div className={cx("scroll-title")}>스크롤타이틀2</div>
-        <div className={cx("scroll-title")}>스크롤타이틀3</div>
-        <div className={cx("scroll-title")}>스크롤타이틀4</div>
-        <div className={cx("scroll-title")}>스크롤타이틀5</div>
-        <div className={cx("scroll-title")}>스크롤타이틀6</div>
-        <div className={cx("scroll-title")}>스크롤타이틀7</div>
-        <div className={cx("scroll-title")}>스크롤타이틀8</div>
-        <div className={cx("scroll-title")}>스크롤타이틀9</div>
-        <div className={cx("scroll-title")}>스크롤타이틀10</div>
+      <div className={cx("title-img")}></div>
+      <div className={cx("title-box")}>
+        <div className={cx("fixed-title")}>이것은</div>
+        <div className={cx("scroll-title-box")}>
+          {title.map((val, idx) => {
+            return (
+              <div
+                key={idx}
+                className={cx(
+                  "scroll-title",
+                  activeIdx === idx ? "active" : ""
+                )}
+              >
+                {val}
+              </div>
+            );
+          })}
+        </div>
+        <div className={cx("fixed-title")}>고정된 텍스트</div>
       </div>
-      <div className={cx("fixed-title")}>고정된 텍스트</div>
     </div>
   );
 };
